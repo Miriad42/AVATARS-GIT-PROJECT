f4a2ecaea9789c992f5853d8ef3862be6502325d
Miriad42 <vladyslavvakhutko@gmail.com>
2024-06-26 18:26:31 -0700

    Here we go

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -4,5 +4,8 @@ public class Testing {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("I want to get a job with 140k$+/year salary");
+        System.out.println("Soon as possible");
+        System.out.println("And I will because I can");
     }
 }
